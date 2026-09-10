ae2fcf9e26102d93897baadffd9d3698cccfd5f0
Santhosh .I 🦇 <santhoshilaiyaraja2006@gmail.com>
2026-09-10 16:11:15 +0530

    Time: 43 ms (79.62%) | Memory: 19.2 MB (57.43%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.py
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -4,7 +4,7 @@ class Solution:
         a , b = 0 , 1
         fibo = []
 
-        for i in range(31):
+        for i in range(30):
             fibo.append(a)
             a , b = b , a + b
         
