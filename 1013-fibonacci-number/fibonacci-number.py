527d8ab248c2914a2bec6e51be7d939adbd26003
Santhosh .I 🦇 <santhoshilaiyaraja2006@gmail.com>
2026-09-10 16:09:35 +0530

    Time: 37 ms (94.78%) | Memory: 19.2 MB (19.22%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -0,0 +1,17 @@
+class Solution:
+    def fib(self, n: int) -> int:
+
+        a , b = 0 , 1
+        fibo = []
+
+        for i in range(31):
+            fibo.append(a)
+            a , b = b , a + b
+        
+        if n <= 1:
+            return n
+        else:
+            return fibo[n - 1] + fibo[n - 2]
+            
+
+        
